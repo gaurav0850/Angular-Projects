bc5192d4ffb7bfac17b1ccae5129f5f2d8271479
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-07-23 09:09:44 +0000

    Add HeaderComponent to app imports, how to add custom component to main app

--- essentials/src/main.ts
+++ essentials/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+//This is one of the ways to show component on UI, but it's not recommended
+// import { HeaderComponent } from './app/header.component';
+// bootstrapApplication(HeaderComponent);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
