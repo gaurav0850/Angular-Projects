7ead37a167dd39f5fbe274dacab13249b62580f3
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-07-23 09:27:13 +0000

    style and image

--- essentials/src/app/header.component.ts
+++ essentials/src/app/header.component.ts
@@ -5,7 +5,10 @@ import { Component, signal } from '@angular/core';
     selector: 'app-header',
     standalone: true, //by default true in angular 19+ & not needed to define
     //   template: '<h1>Hello world </h1>', this can also work but not recommended
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    //different properties for css
+    //style, styles, styleUrl, styleUrls
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent {
