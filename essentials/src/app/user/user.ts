c657ec37c2322995bf906da0f7b43f3007d1643e
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-07-24 16:35:35 +0000

    removed input signal to use decorators

--- essentials/src/app/user/user.ts
+++ essentials/src/app/user/user.ts
@@ -7,18 +7,12 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.css'
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() { }
 }
