2727177d6416070f4ae76bfffc27dbd06fd7e45d
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-08-02 05:33:49 +0000

    Add Tasks component and integrate into app

--- essentials/src/app/app.ts
+++ essentials/src/app/app.ts
@@ -2,18 +2,25 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user';
 import { DUMMY_USERS } from './dummy-users'
+import { TasksComponent } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('essentials');
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
     console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
