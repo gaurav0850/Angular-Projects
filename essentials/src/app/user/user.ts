154c9ab02f3e0ef7f4b403043d9b59b6e5dd7be6
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-08-02 05:55:38 +0000

    interface instead of type

--- essentials/src/app/user/user.ts
+++ essentials/src/app/user/user.ts
@@ -1,6 +1,12 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 
-type User = {
+// type User = { 
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
   id: string;
   avatar: string;
   name: string;
