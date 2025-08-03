53800b65e453f56344c78d867aa9d744189dd5d5
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-08-03 14:10:39 +0000

    use localStorage to save data to browser localstorage

--- essentials/src/app/tasks/tasks.service.ts
+++ essentials/src/app/tasks/tasks.service.ts
@@ -4,6 +4,14 @@ import { type NewTaskData } from "./task/task.model";
 @Injectable({ providedIn: 'root' })
 export class TasksService { //export to use it in other ts files.
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -16,10 +24,16 @@ export class TasksService { //export to use it in other ts files.
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
   private tasks = [
