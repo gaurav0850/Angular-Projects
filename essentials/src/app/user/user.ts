a6534c1f03e96f41ee2c6e26f51e346745f1b151
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-08-03 08:05:09 +0000

    Dynamic css class binding

--- essentials/src/app/user/user.ts
+++ essentials/src/app/user/user.ts
@@ -12,6 +12,7 @@ export class UserComponent {
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
   @Input({ required: true }) user !: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
