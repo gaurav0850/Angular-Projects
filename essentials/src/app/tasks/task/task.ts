1582720ba5049486e85138c41f8d54beab897e66
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-08-03 13:24:18 +0000

    DatePipe for displaying date

--- essentials/src/app/tasks/task/task.ts
+++ essentials/src/app/tasks/task/task.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { TaskInterface } from './task.model';
 import { Card } from '../../shared/card/card';
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css'
 })
