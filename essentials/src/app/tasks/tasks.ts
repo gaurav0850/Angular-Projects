3623a930d3661d796791d9a9891341418e75d16c
gaurav0850 <118876804+gaurav0850@users.noreply.github.com>
2025-08-03 13:37:15 +0000

    implement TasksService for handling user tasks and add task functionality

--- essentials/src/app/tasks/tasks.ts
+++ essentials/src/app/tasks/tasks.ts
@@ -16,12 +16,12 @@ export class TasksComponent {
   isAddingTask = false;
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+
   }
 
   onCompleteTask(id: string) {
-    this.tasks= this.tasks.filter((task) => task.id !== id);
-  } 
+
+  }
 
   onStartAddTask() {
     this.isAddingTask = true;
@@ -32,37 +32,6 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId, 
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-    });
-    this.isAddingTask=false;
+    this.isAddingTask = false;
   }
-
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master angular',
-      summary: 'Learn all the basic and advanced features of Angular.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2025-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2025-06-15',
-    }
-  ];
 }
