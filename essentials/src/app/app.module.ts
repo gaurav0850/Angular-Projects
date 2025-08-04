71625d0ca86df3992f6db5464b97ce900bae319b
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-08-04 11:40:57 +0530

    added other components in module. application working

--- essentials/src/app/app.module.ts
+++ essentials/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { App } from './app';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user';
+import { TasksComponent } from './tasks/tasks';
 
 @NgModule({
-  declarations: [App],
+  declarations: [App], //for non standalone components
   bootstrap: [App],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], //for standalone components
 })
 export class AppModule {}
