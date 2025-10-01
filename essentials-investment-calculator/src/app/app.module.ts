00be3a85065f9708a8674df2b3e275495e2c0f27
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-01 15:21:10 +0530

    Refactor app module to use UserInputModule and remove FormsModule

--- essentials-investment-calculator/src/app/app.module.ts
+++ essentials-investment-calculator/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { App } from './app';
 import { Header } from './header/header';
 import { InvestmentResults } from './investment-results/investment-results';
-import { UserInput } from './user-input/user-input';
+import { UserInputModule } from './user-input/user-input.module';
 
 @NgModule({
-  declarations: [App, Header, UserInput, InvestmentResults],
-  imports: [BrowserModule, FormsModule],
+  declarations: [App, Header, InvestmentResults],
+  imports: [BrowserModule, UserInputModule],
   bootstrap: [App]
 })
 export class AppModule {}
