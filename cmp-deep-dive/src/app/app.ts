6ab720b48f918b3a9af14f4d76e01ca969183abd
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-01 19:28:55 +0530

    Used Dashboard-item for repetitive header and image

--- cmp-deep-dive/src/app/app.ts
+++ cmp-deep-dive/src/app/app.ts
@@ -3,11 +3,12 @@ import { Header } from './header/header';
 import { ServerStatus } from "./dashboard/server-status/server-status";
 import { Traffic } from "./dashboard/traffic/traffic";
 import { Tickets } from './dashboard/tickets/tickets';
+import { DashboardItem } from "./dashboard/dashboard-item/dashboard-item";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [Header, ServerStatus, Traffic, Tickets],
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
 })
 export class AppComponent {
   
