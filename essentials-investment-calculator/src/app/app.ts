d633ad206c3afa02cccff76a764b560e34e0aa4d
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-01 12:28:37 +0530

    Implement investment calculation logic in App component and add investment results helper code

--- essentials-investment-calculator/src/app/app.ts
+++ essentials-investment-calculator/src/app/app.ts
@@ -6,8 +6,34 @@ import { UserInput } from './user-input/user-input';
   selector: 'app-root',
   imports: [Header, UserInput],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('essentials-investment-calculator');
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    annualInvestment: number;
+    duration: number;
+    expectedReturn: number;
+  }) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
 }
