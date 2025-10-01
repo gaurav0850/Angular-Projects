5052da0c59f5f3b72efc8fd690b2a585850c1e54
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-01 18:26:35 +0530

    moved header related code to header component

--- cmp-deep-dive/src/app/app.ts
+++ cmp-deep-dive/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [Header],
 })
 export class AppComponent {
   dummyTrafficData = [
