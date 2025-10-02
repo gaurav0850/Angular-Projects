a242d15239cff3e8a85c838ad022e574c8f0959d
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-02 10:46:33 +0530

    109 Refactor new ticket form to use app-control component for better structure and maintainability

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button";
+import { Control } from "../../../shared/control/control";
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, Control],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css'
 })
