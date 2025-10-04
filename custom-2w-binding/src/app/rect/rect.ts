942da669d4ba412383c9b15c5ce71a189fca92e2
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-05 00:18:44 +0530

    147 modern way of using 2w binding

--- custom-2w-binding/src/app/rect/rect.ts
+++ custom-2w-binding/src/app/rect/rect.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, model } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,21 +9,32 @@ import { Component, input, output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  size = input.required<{
-    width: string;
-    height: string;
-  }>();
+  // size = input.required<{
+  //   width: string;
+  //   height: string;
+  // }>();
 
-  // name should be same as below
-  sizeChange = output<{
+  // // name should be same as below
+  // sizeChange = output<{
+  //   width: string;
+  //   height: string;
+  // }>();
+
+  // Alternative for 2 way binding: easier way in angular 17.2+
+  size = model.required<{
     width: string;
     height: string;
   }>();
 
   onReset() {
-    this.sizeChange.emit({
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100',
+    // });
+
+    this.size.set({
       width: '200',
-      height: '100'
-    });
+      height: '100',
+    })
   }
 }
