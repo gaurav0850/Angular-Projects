70fc8f0310293942b34c7327c2368a9ba0706e3c
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-04 20:09:01 +0530

    refactor: update form handling to use viewChild.required for Angular 17+

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button';
 import { Control } from '../../../shared/control/control';
@@ -10,11 +10,14 @@ import { Control } from '../../../shared/control/control';
   styleUrl: './new-ticket.css',
 })
 export class NewTicket {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // alternative to above code, available only in latest angular 17 or newer
+  // user ViewChildren to select more than one elements
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
