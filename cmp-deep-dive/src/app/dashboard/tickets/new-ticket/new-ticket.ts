f4784bd6dd51106fb62ccbd9f587b9ca8aaa0f5b
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-04 19:44:25 +0530

    128. template variable

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ButtonComponent } from "../../../shared/button/button";
-import { Control } from "../../../shared/control/control";
+import { ButtonComponent } from '../../../shared/button/button';
+import { Control } from '../../../shared/control/control';
 
 @Component({
   selector: 'app-new-ticket',
   imports: [ButtonComponent, Control, FormsModule],
   templateUrl: './new-ticket.html',
-  styleUrl: './new-ticket.css'
+  styleUrl: './new-ticket.css',
 })
 export class NewTicket {
-  onSubmit() {
-    console.log('submitted');
-    
+  onSubmit(titleElement: HTMLInputElement) {
+    console.dir(titleElement);
+    const enteredTitle = titleElement.value;
+    console.log(enteredTitle);
   }
 }
