2a0a3b0234ecb38bcc55a1fb9efd0088e715c4b2
gaurav0850 <gauravwadibhasme0850@gmail.com>
2025-10-04 19:32:27 +0530

    127. ngSubmit() form

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from "../../../shared/button/button";
 import { Control } from "../../../shared/control/control";
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent, Control],
+  imports: [ButtonComponent, Control, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css'
 })
 export class NewTicket {
-
+  onSubmit() {
+    console.log('submitted');
+    
+  }
 }
